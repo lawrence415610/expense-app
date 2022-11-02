03837e2a769c8f5067f46de77959062c3f035854
lawrence415610 <lawrence415610@gmail.com>
2022-11-03 10:44:46 +1100

    main finished

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpenseList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [year, setYear] = useState("2022");
@@ -15,11 +16,10 @@ function Expenses(props) {
     return items.date.getFullYear().toString() === year;
   });
 
-
-
   return (
     <Card className="expenses">
       <ExpensesFilter selected={year} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expenses={filterYearItems} />
       <ExpenseList filterYear={filterYearItems} />
     </Card>
   );
